29522c437b4a25d668ef3eb444b4b9f1df3fca1c
syedzuba1 <syedzubairahm@gmail.com>
2024-12-07 19:01:23 -0500

    fix

--- Prison_Analysis/asgi.py
+++ Prison_Analysis/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for crop_yield_predictor project.
+ASGI config for Prison_Analysis project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_yield_predictor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prison_Analysis.settings')
 
 application = get_asgi_application()
--- Prison_Analysis/wsgi.py
+++ Prison_Analysis/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crop_yield_predictor project.
+WSGI config for Prison_Analysis project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_yield_predictor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prison_Analysis.settings')
 
 application = get_wsgi_application()
